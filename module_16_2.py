9f2e50b0d3880da8f0b15ee5d3899527d787f2d9
GrayThing <samorodov-artem@mail.ru>
2024-12-19 12:53:58 +0300

    Выполнено домашнее задание от 2024/02/17 00:00|Домашнее задание по теме "Валидация данных".

--- module_16_2.py
+++ module_16_2.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Path
+from typing import Annotated
 
 
 app = FastAPI()
@@ -15,11 +16,20 @@ async def admin():
 
 
 @app.get('/user/{user_id}')
-async def by_user_id(user_id: int = Path(ge=1, le=100, description='Enter User ID', example='65')):
+async def by_user_id(user_id: Annotated[int, Path(ge=1,
+                                                  le=100,
+                                                  description='Enter User ID',
+                                                  example='65')]):
     return f'Вы вошли как пользователь {user_id}'
 
 
 @app.get('/user/{username}/{age}')
-async def user_info(username: str = Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser'),
-                    age: int = Path(ge=18, le=120, description='Enter age', example='24')):
+async def user_info(username: Annotated[str, Path(min_length=5,
+                                                  max_length=20,
+                                                  description='Enter username',
+                                                  example='UrbanUser')],
+                    age: Annotated[int, Path(ge=18,
+                                             le=120,
+                                             description='Enter age',
+                                             example='24')]):
     return f'Информация о пользователе. Имя: {username}, Возраст: {age}'
